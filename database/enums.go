7b466974a1da66ccea69586ec72bff6b3c1493c7
m.shrief <m.shrief.dev@proton.me>
2026-07-12 15:43:23 +0300

    Added TimePeriodEnum

--- database/enums.go
+++ database/enums.go
@@ -18,12 +18,14 @@ func CreateEnumsIfNotExists() {
 		if err := tx.Exec(RoleEnum_CreateSQLIfNotExists).Error; err != nil {
 			return err
 		}
+
+		if err := tx.Exec(TimePeriodEnum_CreateSQLIfNotExists).Error; err != nil {
+			return err
+		}
+
 		return nil
 	})
 
-	// if err != nil {
-	// 	logger.Error().Err(err).Msg("Transaction failed")
-	// }
 }
 
 type RoleEnum string
@@ -50,12 +52,12 @@ var RoleEnum_CreateSQLIfNotExists = fmt.Sprintf(
 	`role_enum`, `role_enum`, RoleEnum_Normal, RoleEnum_Banned, RoleEnum_Management, RoleEnum_DBA, RoleEnum_Analytics,
 )
 
-// 2. Implement the driver.Valuer interface to save to DB
+// Implement the driver.Valuer interface to save to DB
 func (r RoleEnum) Value() (driver.Value, error) {
 	return string(r), nil
 }
 
-// 3. Implement the sql.Scanner interface to read from DB
+// Implement the sql.Scanner interface to read from DB
 func (r *RoleEnum) Scan(value interface{}) error {
 	if value == nil {
 		*r = ""
@@ -69,6 +71,57 @@ func (r *RoleEnum) Scan(value interface{}) error {
 		*r = RoleEnum(v)
 	default:
 		return errors.New("invalid type for RoleEnum")
+	}
+	return nil
+}
+
+type TimePeriodEnum string
+
+const (
+	TimePeriodEnum_Undefined  TimePeriodEnum = "غير محدد"
+	TimePeriodEnum_Jahli      TimePeriodEnum = "جاهلي"
+	TimePeriodEnum_Amoei      TimePeriodEnum = "أموي"
+	TimePeriodEnum_Abasi      TimePeriodEnum = "عباسي"
+	TimePeriodEnum_Andalusi   TimePeriodEnum = "أندلسي"
+	TimePeriodEnum_TurkishEra TimePeriodEnum = "عثماني ومملوكي"
+	TimePeriodEnum_Modern     TimePeriodEnum = "حديث"
+)
+
+var TimePeriodEnum_CreateSQLIfNotExists = fmt.Sprintf(
+	`DO $$ BEGIN
+		IF NOT EXISTS (
+			SELECT 1 FROM pg_type 
+			WHERE typname = '%s' 
+			AND typnamespace = 'public'::regnamespace
+		) THEN
+			CREATE TYPE %s AS ENUM ('%s', '%s', '%s', '%s', '%s', '%s', '%s');
+		END IF;
+	END $$;
+	`,
+	`time_period_enum`, `time_period_enum`, TimePeriodEnum_Undefined,
+	TimePeriodEnum_Jahli, TimePeriodEnum_Amoei, TimePeriodEnum_Abasi,
+	TimePeriodEnum_Andalusi, TimePeriodEnum_TurkishEra, TimePeriodEnum_Modern,
+)
+
+// Implement the driver.Valuer interface to save to DB
+func (tp TimePeriodEnum) Value() (driver.Value, error) {
+	return string(tp), nil
+}
+
+// Implement the sql.Scanner interface to read from DB
+func (tp *TimePeriodEnum) Scan(value interface{}) error {
+	if value == nil {
+		*tp = ""
+		return nil
+	}
+	// Postgres often returns []byte, MySQL might return string
+	switch v := value.(type) {
+	case []byte:
+		*tp = TimePeriodEnum(v)
+	case string:
+		*tp = TimePeriodEnum(v)
+	default:
+		return errors.New("invalid type for TimePeriodEnum")
 	}
 	return nil
 }
