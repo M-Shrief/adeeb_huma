195a268b19ffe5712455b58044cd95958c8d6fdc
m.shrief <m.shrief.dev@proton.me>
2026-07-12 15:04:30 +0300

    Added GET / & GET /ping routes, added utils module to contain global reused code and used BasedResponse struct.

--- router/index.go
+++ router/index.go
@@ -1,6 +1,10 @@
 package router
 
 import (
+	"adeeb_huma/utils"
+	"context"
+	"net/http"
+
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
 	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
@@ -26,5 +30,66 @@ func Init() huma.API {
 
 	API = humachi.New(R, config)
 
+	RegisterAllRoutes()
+
 	return API
+}
+
+func RegisterAllRoutes() {
+
+	huma.Register(
+		API,
+		huma.Operation{
+			OperationID: "index",
+			Method:      http.MethodGet,
+			Path:        "/",
+			Summary:     "Server's metadata",
+			Description: "Respond with server's metadata",
+			// Tags:          []string{},
+			DefaultStatus: http.StatusOK,
+		},
+		IndexRouteHandler,
+	)
+
+	huma.Register(
+		API,
+		huma.Operation{
+			OperationID: "ping",
+			Method:      http.MethodGet,
+			Path:        "/ping",
+			Summary:     "Ping the server",
+			Description: "Ping the server to check it's status",
+			// Tags:          []string{},
+			DefaultStatus: http.StatusOK,
+		},
+		PingRouteHandler,
+	)
+
+}
+
+type IndexResponse_JSONBody struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Summary     string `json:"summary"`
+	Version     string `json:"version"`
+	Docs_url    string `json:"docs-url"`
+}
+type IndexResponse struct {
+	Body   IndexResponse_JSONBody
+	Status int
+}
+
+func IndexRouteHandler(ctx context.Context, input *struct{}) (*IndexResponse, error) {
+	body := IndexResponse_JSONBody{
+		Title:       "Adeeb Huma",
+		Description: "An Iteration for Adeeb's RESTful API using Go, Huma and Postgres.",
+		Summary:     "An Iteration for Adeeb's RESTful API using Go",
+		Version:     "0.1.0",
+		Docs_url:    "/docs",
+	}
+	return &IndexResponse{Body: body, Status: http.StatusOK}, nil
+}
+
+func PingRouteHandler(ctx context.Context, input *struct{}) (*utils.BaseResponse, error) {
+	return &utils.BaseResponse{Body: utils.BaseResponse_JSONBody{Message: "Pong"}, Status: http.StatusOK}, nil
 }
