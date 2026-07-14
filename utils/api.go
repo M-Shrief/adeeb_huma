acea658c1de1f12b3a1e613f6aadf19a5c0e37e6
m.shrief <m.shrief.dev@proton.me>
2026-07-14 11:51:03 +0300

    changed the order of structs, to make the parent then embedded structs.

--- utils/api.go
+++ utils/api.go
@@ -1,10 +1,10 @@
 package utils
 
-type BaseResponse_JSONBody struct {
-	Message string `json:"message"`
-}
-
 type BaseResponse struct {
 	Body   BaseResponse_JSONBody
 	Status int
+}
+
+type BaseResponse_JSONBody struct {
+	Message string `json:"message"`
 }
