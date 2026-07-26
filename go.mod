def589b0549f6ba7637bf795091720a81cbbfa20
m.shrief <m.shrief.dev@proton.me>
2026-07-26 23:15:45 +0300

    Added jwt file to deal with JWT creation & validation.

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 
 require (
 	github.com/fxamacker/cbor/v2 v2.9.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.10.0 // indirect
@@ -30,6 +31,7 @@ require (
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
+	golang.org/x/crypto v0.54.0 // indirect
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.40.0 // indirect
---
+++ internal/auth/jwt.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"adeeb_huma/config"
+	"adeeb_huma/database"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type JWTUserClaim struct {
+	ID    string              `json:"id"`
+	Name  string              `json:"name"`
+	Roles []database.RoleEnum `json:"roles"`
+}
+
+func CreateJWT(ttl time.Duration, user JWTUserClaim, permissions []string) (string, error) {
+	now := time.Now().UTC()
+
+	claims := make(jwt.MapClaims)
+	claims["user"] = user               // Our custom data.
+	claims["exp"] = now.Add(ttl).Unix() // The expiration time after which the token must be disregarded.
+	claims["iat"] = now.Unix()          // The time at which the token was issued.
+	claims["permissions"] = permissions
+	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, claims).SignedString(config.JWT_PRIVATE_KEY)
+	if err != nil {
+		return "", fmt.Errorf("create token error: %w", err)
+	}
+
+	return token, nil
+}
+
+func VerifyJWT(authHeader string) (jwt.MapClaims, error) {
+	if authHeader == "" {
+		return nil, fmt.Errorf("Empty header")
+	}
+
+	token := authHeader[7:]
+
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected method: %s", t.Header["alg"])
+		}
+		return config.JWT_PUBLIC_KEY, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("parse error: %v", err)
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	cmp := time.Now().Unix()
+	if !ok || !parsedToken.Valid {
+		return nil, fmt.Errorf("invalid token")
+	} else if claims.VerifyExpiresAt(cmp, true) == false {
+		return nil, fmt.Errorf("error: token is already expired")
+	} else if claims.VerifyIssuedAt(cmp, true) == false {
+		return nil, fmt.Errorf("error: token is issued in invalid time")
+	}
+
+	return claims, nil
+}
