39d0dda43a05eb9e4bb0cf71951a0cf7a8cf2287
m.shrief <m.shrief.dev@proton.me>
2026-07-26 23:16:50 +0300

    Added auth's utils to check user permissions and validate authorization.

---
+++ internal/auth/utils.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+	"adeeb_huma/database"
+	"slices"
+)
+
+type OPEnum string
+
+const (
+	OPEnum_Write OPEnum = "write"
+	OPEnum_Read  OPEnum = "read"
+)
+
+func CreatePermissions(roles []database.RoleEnum, op OPEnum) []string {
+	var permissions []string
+
+	for _, role := range roles {
+		permissions = append(permissions, CreatePermission(role, op))
+	}
+
+	return permissions
+}
+func CreatePermission(role database.RoleEnum, op OPEnum) string {
+	return string(role) + ":" + string(op)
+}
+
+func CheckPermissions(authorized_list, user_permissions []string, op OPEnum) bool {
+	isAuthorized := false
+	isBanned := false
+
+	for _, perm := range user_permissions {
+		if op == OPEnum_Write && perm == CreatePermission(database.RoleEnum_Banned, OPEnum_Write) {
+			isBanned = true
+			break
+		} else if op == OPEnum_Read && perm == CreatePermission(database.RoleEnum_Banned, OPEnum_Read) {
+			isBanned = true
+			break
+		}
+
+		isAuthorized = slices.Contains(authorized_list, perm) || false
+	}
+
+	if isBanned {
+		isAuthorized = false
+	}
+
+	return isAuthorized
+}
