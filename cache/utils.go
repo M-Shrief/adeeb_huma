a7ed7fd64f407fc642175f4474530a29acbd4c20
m.shrief <m.shrief.dev@proton.me>
2026-08-03 19:09:39 +0300

    Added utils for cache module to reduce boilerplate.

---
+++ cache/utils.go
@@ -0,0 +1,56 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/google/uuid"
+)
+
+// Reusuable function to reduce boilerplate dealing with JSON data.
+
+func GetJSON(ctx context.Context, key string) (map[string]any, error) {
+	value, err := Client.Get(context.TODO(), key)
+	if err != nil || value.IsNil() {
+		return nil, err
+	}
+	var data map[string]any
+	json_str := value.Value()
+
+	// Unmarshal the JSON string into the map
+	// Note: jsonStr must be converted to a byte slice []byte
+	err = json.Unmarshal([]byte(json_str), &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// Used to Set JSON data as a string, as I don't need atomic updates.
+func SetJSON(ctx context.Context, key string, value any) error {
+	// Transform map into JSON byte slice
+	json_bytes, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	json_str := string(json_bytes) // Convert byte slice to string
+	_, err = Client.Set(ctx, key, json_str)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DelKey(ctx context.Context, key string) error {
+	_, err := Client.Del(ctx, []string{key})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func FormatKeyByID(prefix string, id uuid.UUID) string {
+	return prefix + ":" + id.String()
+}
